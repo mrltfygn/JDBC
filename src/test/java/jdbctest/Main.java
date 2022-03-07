dfeb6554f339cb1adf32f461af6ae74b5a11d6ed
mrltfygn <omeryegen88@gmail.com>
2022-03-07 13:33:46 +0100

    Initial commit

---
+++ src/test/java/jdbctest/Main.java
@@ -0,0 +1,12 @@
+package jdbctest;
+
+public class Main {
+    public static void main(String[] args) {
+        String dbUrl="jdbc:oracle:thin:@3.89.104.82:1521:xe";
+        String dbUsername="hr";
+        String dbPassword="hr";
+
+
+
+    }
+}
